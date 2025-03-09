9bc3d007c3423ff091320b9ed4b33c5482855079
Siddhi <siddhimahadik257@gmail.com>
2025-03-09 14:26:23 +0530

    Feat: created FilterBar and categoryBar component

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { Header } from './components/header/header';
+import CategoryBar from './components/category/category';
+import { FilterBar } from './components/filterBar/filter';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <FilterBar />
     </div>
   );
 }
